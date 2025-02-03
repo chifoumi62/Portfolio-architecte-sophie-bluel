5833438ab7b379e2f7f1ff0e54cb838c8d8190e8
sebastien <sebastien.denavaut@gmail.com>
2025-02-03 16:15:00 +0100

    creation des bouton filtres

---
+++ FrontEnd/filtres .js
@@ -0,0 +1,23 @@
+//creation des boutons de filtres
+
+const boutonTous=document.createElement("button");
+boutonTous.innerText="Tous";
+boutonTous.setAttribute("class","btn");
+
+const boutonObjets=document.createElement("button");
+boutonObjets.innerText="Objets";
+boutonObjets.setAttribute("class","btn");
+
+const boutonAppartements=document.createElement("button");
+boutonAppartements.innerText="Appartements";
+boutonAppartements.setAttribute("class","btn");
+
+const boutonHotelsRestaurants=document.createElement("button");
+boutonHotelsRestaurants.innerText="Hotels & restaurants";
+boutonHotelsRestaurants.setAttribute("class","btn");
+
+const sectionFiltres=document.querySelector(".filtres");
+sectionFiltres.appendChild(boutonTous);
+sectionFiltres.appendChild(boutonObjets);
+sectionFiltres.appendChild(boutonAppartements);
+sectionFiltres.appendChild(boutonHotelsRestaurants);
