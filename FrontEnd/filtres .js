452f33f7904768c56718199b9970bd77e99504cc
sebastien <sebastien.denavaut@gmail.com>
2025-02-10 15:26:21 +0100

    gestion des boutons (fin ticket 2)

--- FrontEnd/filtres .js
+++ FrontEnd/filtres .js
@@ -1,7 +1,11 @@
+let reponseBody=await fetch(`http://localhost:5678/api/works`);
+let reponse=await reponseBody.json();
+
 import { genererElement } from "./fonction.js";
+
 //appel de la fonction importer
 
-genererElement();
+genererElement(reponse);
 
 //creation des boutons de filtres
 
@@ -11,18 +15,51 @@ boutonTous.setAttribute("class","btn");
 
 const boutonObjets=document.createElement("button");
 boutonObjets.innerText="Objets";
-boutonObjets.setAttribute("class","btn");
+boutonObjets.setAttribute("class","btn1");
 
 const boutonAppartements=document.createElement("button");
 boutonAppartements.innerText="Appartements";
-boutonAppartements.setAttribute("class","btn");
+boutonAppartements.setAttribute("class","btn2");
 
 const boutonHotelsRestaurants=document.createElement("button");
 boutonHotelsRestaurants.innerText="Hotels & restaurants";
-boutonHotelsRestaurants.setAttribute("class","btn");
+boutonHotelsRestaurants.setAttribute("class","btn3");
 
 const sectionFiltres=document.querySelector(".filtres");
 sectionFiltres.appendChild(boutonTous);
 sectionFiltres.appendChild(boutonObjets);
 sectionFiltres.appendChild(boutonAppartements);
 sectionFiltres.appendChild(boutonHotelsRestaurants);
+
+// gestion des boutons filtres
+
+    boutonObjets.addEventListener("click",function() {
+    const reponseObjets=reponse.filter(function(reponse){
+        return reponse.categoryId===1;
+    });
+    
+    document.querySelector(".gallery").innerHTML="";
+    genererElement(reponseObjets);
+});
+
+boutonAppartements.addEventListener("click",function() {
+    const reponseAppartements=reponse.filter(function(reponse){
+        return reponse.categoryId===2;
+    });
+    
+    document.querySelector(".gallery").innerHTML="";
+    genererElement(reponseAppartements);
+});
+
+boutonHotelsRestaurants.addEventListener("click",function() {
+    const reponseHRest=reponse.filter(function(reponse){
+        return reponse.categoryId===3;
+    });
+    
+    document.querySelector(".gallery").innerHTML="";
+    genererElement(reponseHRest);
+});
+
+boutonTous.addEventListener("click",function() {
+    genererElement(reponse);
+});
